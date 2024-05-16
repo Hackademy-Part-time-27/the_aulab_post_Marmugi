bf61868e3f06bccb9151398f5af914b785c2fa8a
Ivan Marmugi <ivanmarmugi@gmail.com>
2024-05-16 22:27:06 +0200

    Articoli

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,15 +14,27 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Ciao {{ Auth::user()->name}}
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Profilo</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#" noclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a></li>
+            <form action="{{route('logout')}}" method="POST" id="form-logout">
+                @csrf
+            </form>
           </ul>
         </li>
+        @endauth
+        @guest
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Benvenuto Ospite</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
+                <li><a class="dropdown-item" href="{{route('login')}}">Accedi</a></li>
+            </ul>
+        </li>
+        @endguest
         <li class="nav-item">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
